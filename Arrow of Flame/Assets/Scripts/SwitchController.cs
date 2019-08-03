b49b5be8c83d0c2e80fd0520304cb4f0341a3752
DESKTOP-LHFR5BH\Andy <richtertest@gmail.com>
2019-08-03 13:24:22 -0400

    Switches

---
+++ Arrow of Flame/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    //public variables
+    public GameObject door;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //do anything else we want
+            door.SetActive(false);
+        }
+    }
+}
