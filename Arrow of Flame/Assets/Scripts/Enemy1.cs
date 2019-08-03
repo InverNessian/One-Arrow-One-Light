39655ed771370752e0b67d0388d805e3fef0f19c
Migueldizona <53281114+Migueldizona@users.noreply.github.com>
2019-08-03 02:30:45 +0100

    Enemy1

---
+++ Arrow of Flame/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    public Transform Player;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = UnityEngine.Vector3.MoveTowards(transform.position, Player.position, speed*Time.deltaTime);
+    }
+}
