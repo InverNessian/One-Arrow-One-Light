5054a1b10807bb741d654899859794ac29a42732
DESKTOP-LHFR5BH\Andy <richtertest@gmail.com>
2019-08-04 12:25:16 -0400

    Added Credits

---
+++ Arrow of Flame/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneController : MonoBehaviour
+{
+    public GameObject collar;
+    public GameObject cave;
+    public GameObject button;
+
+    private void Start()
+    {
+        StartCoroutine(Transition());
+    }
+
+
+    IEnumerator Transition()
+    {
+        //after we load the new scene, set it to inactive so we can click other button UIs
+        yield return new WaitForSeconds(7f);
+        cave.GetComponent<Animator>().SetBool("Transition", true);
+        Debug.Log(cave.GetComponent<Animator>().GetBool("Transition"));
+        yield return new WaitForSeconds(1f);
+        cave.GetComponent<Animator>().SetBool("Pan", true);
+        button.SetActive(true);
+    }
+
+
+}
---
+++ Arrow of Flame/Assets/Scripts/Victory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("GameController").GetComponent<GameController>().LoadScene(5);
+        }
+    }
+}
